63dbd860c4728becb5ad66c3ec1d999c8caecddf
Son, Tran Vu Thanh <tranvthanhson@gmail.com>
2017-09-28 13:28:21 +0700

    @change style nav bar

--- views/nav_bar.php
+++ views/nav_bar.php
@@ -1,15 +1,15 @@
 <div class="nav-bar">
 	<div class="container">
 		<ul>
-			<li><a class="item" href="">Home</a></li>
-			<li><a class="item" href="">Sale</a></li>
-			<li><a class="item" href="">Handbags</a></li>
-			<li><a class="item" href="">Wallets</a></li>
-			<li><a class="item" href="">Accessories</a></li>
-			<li><a class="item" href="">Menstore</a></li>
-			<li><a class="item" href="">Shoes</a></li>
-			<li><a class="item" href="">Vintage</a></li>
-			<li><a class="item" href="">Services</a></li>
+			<li><a class="item1" href="">Home</a></li>
+			<li><a class="item1" href="">Sale</a></li>
+			<li><a class="item1" href="">Handbags</a></li>
+			<li><a class="item1" href="">Wallets</a></li>
+			<li><a class="item1" href="">Accessories</a></li>
+			<li><a class="item1" href="">Menstore</a></li>
+			<li><a class="item1" href="">Shoes</a></li>
+			<li><a class="item1" href="">Vintage</a></li>
+			<li><a class="item1" href="">Services</a></li>
 		</ul>
 	</div>
 </div>
